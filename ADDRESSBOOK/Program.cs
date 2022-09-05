694169a61ed05c2b65b4138086260aa73a021c5b
sayalipagar <pagarsayali565@gmail.com>
2022-09-05 14:37:58 +0530

    [sayali]Add:CreateContact

--- ADDRESSBOOK/Program.cs
+++ ADDRESSBOOK/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Contact contact = new Contact()
+            {
+                FirstName = "Sayali",
+                LastName = "Nikam",
+                Address = "Pune",
+                City ="pune",
+                State = "Maharastra",
+                Zip = 411061,
+                PhoneNo="879595869",
+                Email = "sayalipagar@gmail.com"
+
+            };
+            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNo + "\n" + contact.Email);
         }
     }
 }
